af59b3e8908ececa2caa1bc47b02dcc0195205ab
Sayed Haneef <sayedhaneef31@gmail.com>
2025-03-28 16:45:36 +0530

    Initial commit

---
+++ src/main/java/com/Haneef_Assigmnet/SayedHaneefAssigment_Java/SayedHaneefAssigmentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.Haneef_Assigmnet.SayedHaneefAssigment_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayedHaneefAssigmentJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayedHaneefAssigmentJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Haneef_Assigmnet/SayedHaneefAssigment_Java/SayedHaneefAssigmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Haneef_Assigmnet.SayedHaneefAssigment_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayedHaneefAssigmentJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
